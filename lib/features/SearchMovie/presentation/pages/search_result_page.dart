24e1bc473d01fe08277b4d8ce2d0054e632d0931
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-06-18 21:57:59 +0100

    ui pt 4

--- lib/features/SearchMovie/presentation/pages/search_result_page.dart
+++ lib/features/SearchMovie/presentation/pages/search_result_page.dart
@@ -4,8 +4,6 @@ import 'package:movie_browser/features/SearchMovie/data/models/movie_brief_hive_
 import 'package:movie_browser/features/SearchMovie/presentation/bloc/movie_search_bloc/movie_search_bloc.dart';
 import 'package:movie_browser/features/SearchMovie/presentation/widgets/search_box.dart';
 
-import '../../../../injection_container.dart';
-
 class SearchResultPage extends StatefulWidget {
   static const routeName = '/results';
 
@@ -15,18 +13,18 @@ class SearchResultPage extends StatefulWidget {
 
 class _SearchResultPageState extends State<SearchResultPage> {
   MovieSearchBloc bloc;
+  double appBarSize = 50;
 
   void addSearchEvent(MovieSearchBloc bloc) {
     bloc.add(SearchMovieEvent());
     Navigator.of(context).pushNamed(SearchResultPage.routeName);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    bloc = BlocProvider.of<MovieSearchBloc>(context);
-
-    return Scaffold(
-      appBar: AppBar(
+  Widget _appBar(BuildContext context, MovieSearchBloc bloc, Function callback,
+      double appBarSize) {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(appBarSize),
+      child: AppBar(
         title: SearchBox(small: true),
         elevation: 0,
         backgroundColor: Colors.blueGrey,
@@ -39,26 +37,121 @@ class _SearchResultPageState extends State<SearchResultPage> {
               ),
               width: MediaQuery.of(context).size.width * 0.05,
             ),
-            onPressed: () => addSearchEvent(bloc),
+            onPressed: () => callback(bloc),
           )
         ],
       ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context, MovieSearchBloc bloc) {
+    final state = bloc.state;
+    print('in this function');
+
+    if (state is MovieSearchInitial) {
+      print('Moviesearcchinitial');
+      return Container();
+    } else if (state is SearchLoading) {
+      print('loading');
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else if (state is SearchLoaded) {
+      print('loaded');
+      return _buildLoadedState(context, state);
+    } else {
+      return Container(child: Text('State is $state'));
+    }
+  }
+
+  Widget _buildLoadedState(BuildContext context, SearchLoaded state) {
+    print('loaded state');
+    return Column(
+      children: [
+        Expanded(
+          child: Container(
+            child: ListView.builder(
+              itemCount: state.searchResult.results.length,
+              itemBuilder: (BuildContext ctx, int index) {
+                final MovieBriefHive item = state.searchResult.results[index];
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  color: Colors.white,
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Container(
+                    height: 170,
+                    width: double.infinity,
+                    child: Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(15),
+                              topLeft: Radius.circular(15)),
+                          child: Container(
+                            height: double.infinity,
+                            color: Colors.grey,
+                            width: MediaQuery.of(context).size.width * .3,
+                            child: Image(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(item.poster),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width * .56,
+                                child: Text(
+                                  item.title,
+                                  maxLines: 4,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text('${item.year}'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // ListTile(
+  //                 leading: Image(
+  //                     fit: BoxFit.cover, image: NetworkImage(item.poster)),
+  //                 title: Text(item.title),
+  //                 subtitle: Text('${item.year}'),
+  //               );
+
+  @override
+  Widget build(BuildContext context) {
+    bloc = BlocProvider.of<MovieSearchBloc>(context);
+
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: _appBar(context, bloc, addSearchEvent, appBarSize),
       body: BlocBuilder<MovieSearchBloc, MovieSearchState>(
-        builder: (context, state) {
-          print('before the if statements');
-          if (state is MovieSearchInitial) {
-            print('Moviesearcchinitial');
-            return Container();
-          } else if (state is SearchLoading) {
-            print('loading');
-            return CircularProgressIndicator();
-          } else if (state is SearchLoaded) {
-            print('loaded');
-            return _buildLoadedState(context, state);
-          } else {
-            return Container(child: Text('State is $state'));
-          }
-        },
+        builder: (context, state) => _buildBody(
+          context,
+          bloc,
+        ),
       ),
     );
   }
@@ -68,46 +161,4 @@ class _SearchResultPageState extends State<SearchResultPage> {
     bloc.close();
     super.dispose();
   }
-}
-
-// Widget _buildBody(BuildContext context, MovieSearchBloc bloc) {
-//   final state = bloc.state;
-
-//   if (state is MovieSearchInitial) {
-//     print('Moviesearcchinitial');
-//     return Container();
-//   } else if (state is SearchLoading) {
-//     print('loading');
-//     return Center(
-//       child: CircularProgressIndicator(),
-//     );
-//   } else if (state is SearchLoaded) {
-//     print('loaded');
-//     return ListView.builder(itemBuilder: null);
-//   } else {
-//     return Container(child: Text('State is $state'));
-//   }
-// }
-
-Widget _buildLoadedState(BuildContext context, SearchLoaded state) {
-  print('loaded state');
-  return Column(
-    children: [
-      Container(
-        height: MediaQuery.of(context).size.height * 0.8,
-        child: ListView.builder(
-          itemCount: state.searchResult.results.length,
-          itemBuilder: (BuildContext ctx, int index) {
-            final MovieBriefHive item = state.searchResult.results[index];
-            return ListTile(
-              leading:
-                  Image(fit: BoxFit.cover, image: NetworkImage(item.poster)),
-              title: Text(item.title),
-              subtitle: Text('${item.year}'),
-            );
-          },
-        ),
-      ),
-    ],
-  );
 }
