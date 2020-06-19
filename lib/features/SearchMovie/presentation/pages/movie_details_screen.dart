62a73ee66708ec1390452832a33ce072736daee1
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-06-19 15:17:27 +0100

    ui part 5

--- lib/features/SearchMovie/presentation/pages/movie_details_screen.dart
+++ lib/features/SearchMovie/presentation/pages/movie_details_screen.dart
@@ -1,10 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_browser/features/SearchMovie/data/models/movie_detailed_hive_model.dart';
 import 'package:movie_browser/features/SearchMovie/presentation/bloc/movie_search_bloc/movie_search_bloc.dart';
 import 'package:movie_browser/features/SearchMovie/presentation/pages/search_result_screen.dart';
 
 class MovieDetailsScreen extends StatelessWidget {
   static const String routeName = '/details';
+
+  final double titleShadowOffset = 0.7;
+  final Color titleShadowColor = Colors.white;
+
+  final double sideShadowOffset = 0.1;
+  final Color sideShadowColor = Colors.white;
+
+  TextStyle _sideTextstyle() {
+    return TextStyle(
+      fontSize: 18,
+      color: Colors.grey[900],
+      fontWeight: FontWeight.w600,
+      shadows: [
+        ..._buildShadow(sideShadowOffset, sideShadowColor),
+      ],
+      fontStyle: FontStyle.italic,
+    );
+  }
+
+  List<Shadow> _buildShadow(offset, color) {
+    return [
+      Shadow(
+        offset: Offset(offset, offset),
+        color: color,
+      ),
+      Shadow(
+        offset: Offset(offset, -offset),
+        color: color,
+      ),
+      Shadow(
+        offset: Offset(-offset, offset),
+        color: color,
+      ),
+      Shadow(
+        offset: Offset(-offset, -offset),
+        color: color,
+      ),
+    ];
+  }
+
+  Container _buildInfoOnSides(
+    BuildContext context,
+    dynamic info1,
+    dynamic info2,
+  ) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 1,
+            ),
+            child: Text(
+              '$info1',
+              style: _sideTextstyle(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 1,
+            ),
+            child: Container(
+              alignment: Alignment.centerRight,
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: Text(
+                '$info2',
+                style: _sideTextstyle(),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   Text _buildTitle(MovieSearchState state) {
     if (state is DetailsLoaded) {
@@ -38,12 +118,89 @@ class MovieDetailsScreen extends StatelessWidget {
   }
 
   Widget _buildLoaded(BuildContext context, DetailsLoaded state) {
-    return Center(
-      child: Column(
+    final MovieDetailedHive details = state.movieDetailed;
+
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.9,
+      child: Stack(
         children: [
-          Text('Details Loaded'),
-          Text('${state.movieDetailed.id}'),
-          Text('${state.movieDetailed.title}'),
+          Container(
+            width: double.infinity,
+            child: Image(
+              fit: BoxFit.cover,
+              image: NetworkImage('${details.poster}'),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Colors.white,
+                    Colors.white.withOpacity(0.96),
+                    Colors.white.withOpacity(0),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    '${details.title}',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        ..._buildShadow(titleShadowOffset, titleShadowColor),
+                      ],
+                    ),
+                    maxLines: 3,
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                SizedBox(height: 10),
+                _buildInfoOnSides(
+                  context,
+                  details.year,
+                  details.language,
+                ),
+                _buildInfoOnSides(
+                  context,
+                  '${details.runTime} min',
+                  details.rated,
+                ),
+                _buildInfoOnSides(
+                  context,
+                  '${details.rating}/100',
+                  details.genre,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    details.plot,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -77,7 +234,7 @@ class MovieDetailsScreen extends StatelessWidget {
 
   Future<bool> _onBackPressed(BuildContext context, MovieSearchBloc bloc) {
     bloc.add(SearchMovieEvent());
-    Navigator.of(context).pushNamed(SearchResultScreen.routeName);
+    Navigator.of(context).pop();
   }
 
   @override
@@ -88,14 +245,20 @@ class MovieDetailsScreen extends StatelessWidget {
       onWillPop: () => _onBackPressed(context, bloc),
       child: BlocBuilder<MovieSearchBloc, MovieSearchState>(
         builder: (context, state) => Scaffold(
-          appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.black),
-              onPressed: () => _onBackPressed(context, bloc),
+          appBar: PreferredSize(
+            preferredSize: Size(
+              double.infinity,
+              50,
             ),
-            backgroundColor: Colors.blueGrey,
-            elevation: 0,
-            title: _buildTitle(state),
+            child: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back, color: Colors.black),
+                onPressed: () => _onBackPressed(context, bloc),
+              ),
+              backgroundColor: Colors.blueGrey,
+              elevation: 0,
+              title: _buildTitle(state),
+            ),
           ),
           body: _buildBody(context, bloc),
         ),
